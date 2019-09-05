5858dfc8b5eee2f8080915b70c461f1ec9d4154d
Andrew Williams <1williamsaj@gmail.com>
2019-09-05 03:15:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,8 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |name, age|
     if min_value == nil || age < min_value
-      min_value = value
-      min_key = key
+      min_value = age
+      min_key = name
     end
   end
   min_key
