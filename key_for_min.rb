179cadca63aefe809d4e1896fb7af3507e4014c7
Andrew Williams <1williamsaj@gmail.com>
2019-09-05 03:31:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
 
-  min_key = nil 
-  min_value = nil
+  min_key = nil # We create variables for the key and values set to 'nil' to account for the default argument of the problem.
+  min_value = nil # If the hash is empty, we simply return the value of 'nil'.
 
-  name_hash.each do |name, age|
-    if min_value == nil || age < min_value
-      min_value = age
+  name_hash.each do |name, age| # Our each method will iterate over both the keys and values found within.
+    if min_value == nil || age > min_value # We create an 'or' conditional: If the value is = 'nil', we set that key equal to 'nil' as well. 
+      min_value = age                      # But, if the first part is false, yet are second part
       min_key = name
     end
   end
